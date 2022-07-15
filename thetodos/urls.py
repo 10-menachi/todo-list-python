85b9263b4ae23004a0ce7a2a1c38b1548d46df2f
chris-droid <chrisdroid@localhost.localdomain>
2022-07-15 23:45:23 +0300

    added registration templates

--- thetodos/urls.py
+++ thetodos/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.TodoView.as_view()),
+    path('', views.TodoView.as_view(), name='home'),
     path('add', views.add_todos, name='add_todos'),
     path('delete/<int:todo_id>/', views.delete_todos, name='delete_todos')
 ]
